9854ca1c673663c88a42576ed2d996d80ec3795c
saumya81 <40277007+saumya81@users.noreply.github.com>
2021-05-28 10:23:05 +0530

    Create seive.cpp

---
+++ seive.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+bool seive[10001]={false};
+int main() {
+	
+	// your code here
+	
+	
+	for(int i=1;i<10001;i++)
+	{
+		if(seive[i]==false)
+		{
+			for(int j=i*i;j<10001;j=j+i)
+			{
+				seive[j]=true;
+			}
+		}
+	}
+	
+		for(int i=2;i<10001;i++){
+			if(seive[i]==false)
+			cout<<i;
+		}
+
+	return 0;
+}
