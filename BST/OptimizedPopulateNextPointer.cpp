407edf8e37935d644611d904c3e5f539055b221a
saumya81 <40277007+saumya81@users.noreply.github.com>
2021-07-12 23:56:13 +0530

    Create OptimizedPopulateNextPointer.cpp

---
+++ BST/OptimizedPopulateNextPointer.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    Node* connect(Node* root) {
+        
+        if(root==NULL)
+            return NULL;
+        queue<Node*> q;
+        q.push(root);
+        q.push(NULL);
+        vector<Node*> v;
+     
+        while(!q.empty())
+        {
+            Node* temp=q.front();
+            q.pop();
+  
+             if(temp!=NULL)
+            {
+                Node* n=q.front(); 
+                temp->next=n;
+                if(temp->left)
+                q.push(temp->left);   
+                
+                if(temp->right)
+                q.push(temp->right);
+  
+            }
+            else
+            {
+                q.push(NULL);
+                if(q.front()==NULL)
+                    break;
+            }
+          
+        }
+        return root;
+    }
+};
