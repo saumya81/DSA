09c1387825d4b83f8bc58a57ca188122ec6de24b
saumya81 <40277007+saumya81@users.noreply.github.com>
2021-08-07 00:24:10 +0530

    Create kthElementOfTwoSortedArray.cpp

---
+++ DivideAndConquer/kthElementOfTwoSortedArray.cpp
@@ -0,0 +1,53 @@
+Given two sorted arrays arr1 and arr2 of size N and M respectively and an element K. The task is to find the element that would be at the k’th position of the final sorted array.
+ 
+
+Example 1:
+
+Input:
+arr1[] = {2, 3, 6, 7, 9}
+arr2[] = {1, 4, 8, 10}
+k = 5
+Output:
+6
+Explanation:
+The final sorted array would be -
+1, 2, 3, 4, 6, 7, 8, 9, 10
+The 5th element of this array is 6.
+
+
+
+
+class Solution{
+    public:
+    int kthElement(int arr1[], int arr2[], int n, int m, int k)
+    {
+        
+        if(n > m) {
+            return kthElement(arr2, arr1, m, n, k); 
+        }
+        
+        int low = max(0,k-m), // if k>len2 then we need to pick some element in the right to make the partition correct
+      int high = min(k,n);// in case if length k<len1 then we cannot pick more element in left .. because the partitioned will be hampered
+        
+        while(low <= high) {
+            int cut1 = (low + high) >> 1; 
+            int cut2 = k - cut1; 
+            int l1 = cut1 == 0 ? INT_MIN : arr1[cut1 - 1]; 
+            int l2 = cut2 == 0 ? INT_MIN : arr2[cut2 - 1];
+            int r1 = cut1 == n ? INT_MAX : arr1[cut1]; 
+            int r2 = cut2 == m ? INT_MAX : arr2[cut2]; 
+            
+            if(l1 <= r2 && l2 <= r1) 
+                return max(l1, l2);
+            
+            else if (l1 > r2) 
+                high = cut1 - 1;
+            
+            else 
+                low = cut1 + 1; 
+            
+        }
+        return 1; 
+        
+    }
+};
