ed6f444b743bf8ef801ec9c6f6d351236e74dbe0
saumya81 <40277007+saumya81@users.noreply.github.com>
2021-08-09 22:18:12 +0530

    Create deleteNodeFromBST.cpp

---
+++ BST/deleteNodeFromBST.cpp
@@ -0,0 +1,50 @@
+Node* inSucc(Node* p , Node* &succParent)
+    {
+        p = p->right;
+        while(p->left!=nullptr)
+        {
+            succParent = p;
+            p = p->left;
+        }
+        return p;
+    }
+Node *deleteNode(Node *root,  int X)
+{
+    if(root==NULL)
+    return root;
+    if(root->data>X)
+    root->left=deleteNode(root->left,X);
+    else if(root->data<X)
+    root->right=deleteNode(root->right,X);
+    else
+    {
+        
+        if(root->right==NULL)
+        {
+            Node* temp=root->left;
+            delete(root);
+            return temp;
+        }
+        else if(root->left==NULL)
+        {
+            Node* temp=root->right;
+            delete(root);
+            return temp;
+        }
+        else
+        {
+           Node* succParent = root;
+                Node* succ = inSucc(root , succParent);
+                
+                swap(root->data ,succ->data);
+            
+                if (succParent != root)
+                    succParent->left = succ->right;
+                else
+                    succParent->right = succ->right;
+                
+                delete (succ); 
+        }
+    }
+    return root;
+}
