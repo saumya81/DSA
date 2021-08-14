750aa1f5a4b39e41fe75b630c9d5965dbfdebc72
saumya81 <40277007+saumya81@users.noreply.github.com>
2021-08-14 11:55:56 +0530

    Update WordBreak.cpp

--- DP/WordBreak.cpp
+++ DP/WordBreak.cpp
@@ -1,3 +1,18 @@
+Given a string s and a dictionary of strings wordDict, return true if s can be segmented into a space-separated sequence of one or more dictionary words.
+
+Note that the same word in the dictionary may be reused multiple times in the segmentation.
+
+ 
+
+Example 1:
+
+Input: s = "leetcode", wordDict = ["leet","code"]
+Output: true
+Explanation: Return true because "leetcode" can be segmented as "leet code".
+
+
+---------------------------------------------
+
 class Solution {
 public:
     bool wordBreak(string str, vector<string>& wordDict) {
