71e88e18acac2770d348ae5d36e7e2c5b9a03f8f
saumya81 <40277007+saumya81@users.noreply.github.com>
2021-08-14 11:55:29 +0530

    Create WordBreak.cpp

---
+++ DP/WordBreak.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool wordBreak(string str, vector<string>& wordDict) {
+        
+        int size = str.length();
+    if (size == 0)   return true;
+ 
+    bool wb[size+1];
+    memset(wb, 0, sizeof(wb));
+        for (int i=1; i<=size; i++)
+    {
+        // if wb[i] is false, then check if current prefix can make it true.
+        // Current prefix is "str.substr(0, i)"
+        if (wb[i] == false && find(wordDict.begin(),wordDict.end(),str.substr(0, i))!=wordDict.end())
+            wb[i] = true;
+ 
+        // wb[i] is true, then check for all substrings starting from
+        // (i+1)th character and store their results.
+        if (wb[i] == true)
+        {
+            // If we reached the last prefix
+            if (i == size)
+                return true;
+ 
+            for (int j = i+1; j <= size; j++)
+            {
+                // Update wb[j] if it is false and can be updated
+                // substring starting from index 'i' and length 'j-i'
+                if (wb[j] == false && find(wordDict.begin(),wordDict.end(),str.substr(i, j-i))!=wordDict.end())
+                    wb[j] = true;
+ 
+                // If we reached the last character
+                if (j == size && wb[j] == true)
+                    return true;
+            }
+        }
+    }
+     return false;
+        
+    }
+};
