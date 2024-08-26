051e198bebaf6b5fd38de94663cbb00e9919eb22
saumya81 <40277007+saumya81@users.noreply.github.com>
2024-08-26 23:21:57 +0530

    Update DetectCycleInDirectedGraph.cpp

--- graph/DetectCycleInDirectedGraph.cpp
+++ graph/DetectCycleInDirectedGraph.cpp
@@ -1,31 +1,34 @@
-//visitedRec is to keep the account of all the previous nodes that are visited
+class Solution
+{
+    public:
+	//Function to detect cycle in a directed graph.
+	bool dfs(int src,vector<int> adj[],vector<bool> &visited,vector<bool> visitedRec)
+	{
+	    visited[src]=true;
+	    visitedRec[src]=true;
+	    for(auto i:adj[src])
+	    {
+	        if(visited[i]==false && dfs(i,adj,visited,visitedRec))
+	        return true;
+	        if(visitedRec[i]==true)
+	        return true;
+	    }
+	    visitedRec[src]=false;
+	    return false;
+	}
+	bool isCyclic(int v, vector<int> adj[]) 
+	{
+	   	vector<bool> visited(v,false);
+	   	vector<bool> visitedRec(v,false);
+	   	for(int i=0;i<v;i++)
+	   	{
+	   	    if(visited[i]==false)
+	   	    {
+	   	        if(dfs(i,adj,visited,visitedRec)==true)
+	   	        return true;
 
-class Solution {
-  private: 
-    bool dfs(int node, int parent, int vis[], vector<int> adj[]) {
-        vis[node] = 1; 
-        // visit adjacent nodes
-        for(auto adjacentNode: adj[node]) {
-            // unvisited adjacent node
-            if(!vis[adjacentNode]) {
-                if(dfs(adjacentNode, node, vis, adj) == true) 
-                    return true; 
-            }
-            // visited node but not a parent node
-            else if(adjacentNode != parent) return true; 
-        }
-        return false; 
-    }
-  public:
-    // Function to detect cycle in an undirected graph.
-    bool isCycle(int V, vector<int> adj[]) {
-       int vis[V] = {0}; 
-       // for graph with connected components 
-       for(int i = 0;i<V;i++) {
-           if(!vis[i]) {
-               if(dfs(i, -1, vis, adj) == true) return true; 
-           }
-       }
-       return false; 
-    }
+	   	    }
+	   	}
+	   	return false;
+	}
 };
